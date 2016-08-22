d7436737f8bdb10ff5b7e82eb9b0aed329210845
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-08-22 14:43:40 +0000

    autocommit

--- lectures/lecture01_01/main.cpp
+++ lectures/lecture01_01/main.cpp
@@ -2,20 +2,12 @@
 #include <string>
 using namespace std;
 
-/*
-This is a very
-long comment
-*/
-
-// the main function
 int main()
 {
-    int tala = 42;  // this is a comment
+    int tala;
+    cout << "Please write a number: " << endl;
+    cin >> tala;
 
-    tala = tala + 5;
-
-    cout << "This is \"something\"" << endl;
-    cout << "or something else\n\n\n\n\n" << endl;
     cout << "Tala = " << tala << endl;
     return 0;
 }
