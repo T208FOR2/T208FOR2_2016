afc1b51db88b30e1cd8ac723df975273ab517fd8
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-08-25 08:50:58 +0000

    autocommit

--- lectures/lecture02_01/main.cpp
+++ lectures/lecture02_01/main.cpp
@@ -7,7 +7,9 @@ int main()
 {
     int a,b;
     cout << "This is the start of the program" << endl;
+    cout << "Please write the first number: ";
     cin >> a;
+    cout << "and the second number: ";
     cin >> b;
     cout << "The numbers are " << a << " and " << b << endl;
     return 0;
