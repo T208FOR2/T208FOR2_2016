c7f0c039b3311670116dd25b5f36a555b1695070
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-08-25 08:42:53 +0000

    autocommit

--- lectures/lecture02_01/main.cpp
+++ lectures/lecture02_01/main.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 int main()
 {
-
+    int a;
+    cout << "This is the start of the program" << endl;
+    cin >> a;
+    cout << "The number is " << a << endl;
     return 0;
 }
