eed964c012134d173fc614ce6ec5b6a091acd8b5
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-08-25 09:55:57 +0000

    autocommit

--- lectures/lecture02_02/main.cpp
+++ lectures/lecture02_02/main.cpp
@@ -5,21 +5,20 @@ int main()
 {
     int tala;
     cin >> tala;
+    cout << "0: " << tala << endl;
 
-    cout << "1: " << tala << endl;
-
-    if (tala = 42) {
+    if (tala == 42) {
+        cout << "1: " << tala << endl;
         cout << "You wrote 42!" << endl;
     }
     else if (tala > 0) {
+        cout << "2: " << tala << endl;
         cout << "Positive!" << endl;
     }
     else {
+        cout << "3: " << tala << endl;
         cout << "You wrote a wrong number" << endl;
     }
-
-    cout << "2: " << tala << endl;
-
 
     return 0;
 }
