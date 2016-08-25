b7a01f7d846871d5630491d74edc1018237cfc62
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-08-25 09:11:10 +0000

    autocommit

--- lectures/lecture02_01/main.cpp
+++ lectures/lecture02_01/main.cpp
@@ -5,11 +5,17 @@ using namespace std;
 
 int main()
 {
+    char a,b;
+    cout << "Write something: " << endl;
+    cin >> a >> b;
+    cout << "You wrote " << a << " and " << b << endl;
+/*
     double d = 0.2;
     double epsilon = 0.000000000000000001;
     cout.precision(20);
     cout << d << endl;
     cout << d + epsilon << endl;
+*/
 /*
     int a,b;
     cout << "This is the start of the program" << endl;
