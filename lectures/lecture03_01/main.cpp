87e6040d1090e864c8d9aa05d7cde633c2eba240
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-08-29 14:34:40 +0000

    autocommit

--- lectures/lecture03_01/main.cpp
+++ lectures/lecture03_01/main.cpp
@@ -9,11 +9,12 @@ int main()
     cout << "How many stars? ";
     cin >> n;
 
-    for ( int i = 0; i < n; i++);
+    for ( int i = 0; i < n; i++)
     {
         cout << "*";
     }
     cout << endl;
+    cout << i << endl;
 
     return 0;
 }
