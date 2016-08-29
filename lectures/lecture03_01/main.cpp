e6d2ba8a785cfda3a76ea80d6d430b0398c77954
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-08-29 13:25:39 +0000

    autocommit

--- lectures/lecture03_01/main.cpp
+++ lectures/lecture03_01/main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main()
 {
+    int numberofhellos;
+    cin >> numberofhellos;
+
+    int counter = 0;
+
+    while (counter < numberofhellos) {
+        cout << "Hello!" << endl;
+    }
 
     return 0;
 }
