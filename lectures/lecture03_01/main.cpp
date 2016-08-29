74923870d82035972a5a4ce71eb46e2dd5b2836b
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-08-29 14:08:19 +0000

    autocommit

--- lectures/lecture03_01/main.cpp
+++ lectures/lecture03_01/main.cpp
@@ -8,7 +8,15 @@ int main()
     cin >> a >> b >> c;
 
     cout << "The largest number is: ";
-
+    if (a >= b && a >= c) {
+        cout << a << endl;
+    }
+    else if (b >= a && b >= c) {
+        cout << b << endl;
+    }
+    else {
+        cout << c << endl;
+    }
 
     return 0;
 }
