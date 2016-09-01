cd3e63a0976049cd04d3753a1b3d6454066fac9b
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-01 09:11:25 +0000

    autocommit

--- lectures/lecture04_02/main.cpp
+++ lectures/lecture04_02/main.cpp
@@ -5,9 +5,17 @@ using namespace std;
 // Function declerations
 void prenta_linu(int hlidrun, int whitespace, int symbols, char takn);
 
+int max_grade(int a, int b, int c, int d);
+
 int main()
 {
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
 
+    prenta_linu(6, 3, 4, '*');
+
+    int staersta;
+    staersta = max_grade(a,b,c,d);
 
     return 0;
 }
