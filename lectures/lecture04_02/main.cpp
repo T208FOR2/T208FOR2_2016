c4f714a4142e59c0eb015167d4d9746130b8d76d
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-01 09:05:22 +0000

    autocommit

--- lectures/lecture04_02/main.cpp
+++ lectures/lecture04_02/main.cpp
@@ -2,9 +2,14 @@
 
 using namespace std;
 
+// Function declerations
+void prenta_linu(int hlidrun, int whitespace, int symbols, char takn);
+
 int main()
 {
 
 
     return 0;
 }
+
+// Function definitions
