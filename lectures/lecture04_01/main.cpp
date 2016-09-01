ff26d5ff1984b40e911627b8863aebd72d07d0be
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-01 08:47:13 +0000

    autocommit

--- lectures/lecture04_01/main.cpp
+++ lectures/lecture04_01/main.cpp
@@ -5,10 +5,17 @@ using namespace std;
 
 int main()
 {
+    cout << "Write a number: ";
     int tala;
     cin >> tala;
     cout << "Square root of " << tala << " is ";
     cout << sqrt(tala) << endl;
+
+    int a,b;
+    cout << "Write two integers:";
+    cin >> a >> b;
+
+    cout << a << " / " << b << " = " << a / b << endl;
 
     return 0;
 }
