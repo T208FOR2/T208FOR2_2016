772e4ef36fec36ccde936186d9e3206cd6190c29
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-05 14:07:28 +0000

    autocommit

--- lectures/lecture05_01/main.cpp
+++ lectures/lecture05_01/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+rectangle(int w, int h, int& area, int& circumference);
 
 int main()
 {
@@ -8,4 +9,7 @@ int main()
     return 0;
 }
 
-
+rectangle(int w, int h, int& area, int& circumference) {
+    area = w*h;
+    circumference = w+w+h+h;
+}
