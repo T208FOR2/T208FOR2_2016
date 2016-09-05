882f45f5cd686cc3b66deaabb6b766e2b271f4f0
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-05 14:03:25 +0000

    autocommit

--- lectures/lecture05_01/main.cpp
+++ lectures/lecture05_01/main.cpp
@@ -1,26 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int somefunction(int &tala1, int tala2, int &tala3);
 
 int main()
 {
-    int a,b,c;
-    cin >> a >> b >> c;
-
-    cout << somefunction(a,b,c) << endl;
-    cout << a << endl;
-    cout << b << endl;
-    cout << c << endl;
 
     return 0;
-}
-
-int somefunction(int &tala1, int tala2, int &tala3) {
-    tala1 = 42;
-    tala2 = 23;
-    tala3 = 17;
-    return tala1+tala2+tala3;
 }
 
 
