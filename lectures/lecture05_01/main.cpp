599e41ae664a240568af2391447725396db8cce2
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-05 13:06:52 +0000

    autocommit

--- lectures/lecture05_01/main.cpp
+++ lectures/lecture05_01/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+
+void initialize_random_generator(int seed);
+int random_int(int max_value);
 
 int main()
 {
+    int seed;
+    cin >> seed;
+    initialize_random_generator(seed);
+
+    int slembitala;
+
+    for (int i = 0; i < 10; i++) {
+        slembitala = random_int(100);
+        cout << slembitala << endl;
+    }
 
     return 0;
+}
+
+
+void initialize_random_generator(int seed) {
+    if (seed < 0) {
+        srand(static_cast<unsigned int>(time(NULL)) );
+    }
+    else {
+        srand(seed);
+    }
+}
+
+int random_int(int max_value) {
+    return rand() % max_value;
 }
