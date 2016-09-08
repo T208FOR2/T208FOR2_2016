03ee2c5bc54cdb9557ebec0079e47c57629aee06
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-08 08:51:34 +0000

    autocommit

--- lectures/lecture06_01/main.cpp
+++ lectures/lecture06_01/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
+    ifstream theinputfile;
+    theinputfile.open("randomtolur.dat");
 
-
+    if (theinputfile.fail()) {
+        cout << "Can't find the file!" << endl;
+        exit(1);
+    }
+    // Do something clever with the file
+    theinputfile.close();
     return 0;
 }
