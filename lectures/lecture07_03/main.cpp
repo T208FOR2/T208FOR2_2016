088ce277a700d7beb362a244f9deedde16c83ea3
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-12 14:38:19 +0000

    autocommit

--- lectures/lecture07_03/main.cpp
+++ lectures/lecture07_03/main.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 const int FJOLDI = 100;
+void initialize_array(int a[], int n);
 
 int main()
 {
     int tolur[FJOLDI];
-    for (int i = 0; i < FJOLDI; i++) {
-        tolur[i] = i*i;
-    }
+
+
 
     for (int i = 0; i < FJOLDI; i++) {
         cout << tolur[i] << endl;
@@ -19,3 +19,11 @@ int main()
 
     return 0;
 }
+
+void initialize_array(int a[], int n) {
+    for (int i = 0; i < FJOLDI; i++) {
+        a[i] = i*i;
+    }
+}
+
+
