d98d874d2945e24aa08269fe0a56bca09ccb6733
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-12 14:13:50 +0000

    autocommit

--- lectures/lecture07_02/main.cpp
+++ lectures/lecture07_02/main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
 
 int main()
 {
     int tala;
     string texti;
+    char takn;
 
     cout << "Write something: ";
-    cin >> tala;
-    cout << "The number is " << tala << endl;
+    cin.get(takn);
+
+    if (isdigit(takn))
+    {
+        cin >> tala;
+        cout << "The number is " << tala << endl;
+    }
+
     cin >> texti;
     cout << "The text is \"" << texti << "\"" << endl;
 
