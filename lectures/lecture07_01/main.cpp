cb07e38948b167b31c6287fb141b83491315e82e
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-12 13:27:03 +0000

    autocommit

--- lectures/lecture07_01/main.cpp
+++ lectures/lecture07_01/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-
+#include <fstream>  // Files!
+#include <cstdlib>  // exit...
+#include <string>
 using namespace std;
 
 int main()
 {
+    ifstream thefile;
+    string thefilename;
+    cout << "Write the filename: ";
+    cin >> thefilename;
+
+    thefile.open(thefilename.c_str());
+
 
     return 0;
 }
+
