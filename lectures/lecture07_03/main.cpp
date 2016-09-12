d89c4fcee5f42762c70e15847cbc20690bafc840
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-12 14:28:09 +0000

    autocommit

--- lectures/lecture07_03/main.cpp
+++ lectures/lecture07_03/main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int tolur[10];
+    for (int i = 0; i < 10; i++) {
+        tolur[i] = i*i;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        cout << tolur[i] << endl;
+    }
+
     return 0;
 }
