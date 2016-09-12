bbb7d234f8beb352904ef2207e15c0948ba6c120
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-12 14:09:44 +0000

    autocommit

--- lectures/lecture07_02/main.cpp
+++ lectures/lecture07_02/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main()
 {
+    int tala;
+    string texti;
+
+    cout << "Write something: ";
+    cin >> tala;
+    cout << "The number is " << tala << endl;
+    cin >> texti;
+    cout << "The text is " << texti << endl;
+
+
 
     return 0;
 }
