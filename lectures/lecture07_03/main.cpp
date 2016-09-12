607b426a7856a24f63b0d22b8dc93a5c8989eec7
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-12 14:40:22 +0000

    autocommit

--- lectures/lecture07_03/main.cpp
+++ lectures/lecture07_03/main.cpp
@@ -8,20 +8,18 @@ void initialize_array(int a[], int n);
 int main()
 {
     int tolur[FJOLDI];
-
+    initialize_array(tolur, FJOLDI);
 
 
     for (int i = 0; i < FJOLDI; i++) {
         cout << tolur[i] << endl;
     }
 
-    cout << tolur[1000] << endl;
-
     return 0;
 }
 
 void initialize_array(int a[], int n) {
-    for (int i = 0; i < FJOLDI; i++) {
+    for (int i = 0; i < n; i++) {
         a[i] = i*i;
     }
 }
