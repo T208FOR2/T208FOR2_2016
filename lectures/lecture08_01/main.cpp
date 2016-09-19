dc59d1f18ebf9dc1cc5da528bd8ac8f9d559f35e
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-19 13:23:35 +0000

    autocommit

--- lectures/lecture08_01/main.cpp
+++ lectures/lecture08_01/main.cpp
@@ -3,11 +3,13 @@
 
 using namespace std;
 
-
+const int MAXLENGD = 1000;
 
 int main()
 {
     string inntak = "";
+    string ord[MAXLENGD];
+    int n = 0;
 
     while (inntak != "exit") {
         cin >> inntak;
