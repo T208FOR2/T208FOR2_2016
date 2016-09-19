176b5745ae0805b7cc3d5c547d50daa359ba3e40
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-19 14:30:19 +0000

    autocommit

--- lectures/lecture08_02/main.cpp
+++ lectures/lecture08_02/main.cpp
@@ -2,11 +2,19 @@
 
 using namespace std;
 
-void upphafsstillabord(char board[][3], int size);
+void upphafsstillabord(char board[][3], int n);
 
 int main()
 {
     char mylla[3][3];
     upphafsstillabord(mylla, 3);
     return 0;
+}
+
+void upphafsstillabord(char board[][3], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 3; j++) {
+            board[i][j] = '_';
+        }
+    }
 }
