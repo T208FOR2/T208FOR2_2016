f5772a17b7b8a649fee77f9357c8566ccac31e50
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-19 13:35:54 +0000

    autocommit

--- lectures/lecture08_01/main.cpp
+++ lectures/lecture08_01/main.cpp
@@ -9,7 +9,7 @@ int main()
 {
     string inntak = "", leitarord;
     string ord[MAXLENGD];
-    int n = 0;
+    int n = 0, index = -1;
 
     while (inntak != "exit") {
         cin >> inntak;
@@ -22,8 +22,11 @@ int main()
 
     cout << "---------------";
 
-    for (int i = n-1; i >= 0; i--) {
-        cout << ord[i] << endl;
+    for (int i = 0; i < n; i++) {
+        if (leitarord == ord[i]) {
+            index = i;
+            break;
+        }
     }
 
 
