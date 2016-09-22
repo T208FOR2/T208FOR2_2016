799603e56b77af986d19cd8506e802b1bf5ca7c9
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-22 10:07:42 +0000

    autocommit

--- lectures/lecture09_02/main.cpp
+++ lectures/lecture09_02/main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 const int MAXFJOLDI = 100;
 
 int main()
 {
+    ifstream infile;
+    infile.open("test.txt");
     string strengur[MAXFJOLDI];
     int tala[MAXFJOLDI];
     int notud_stok = 0;
 
+    while (infile >> strengur[notud_stok]) {
+        notud_stok++;
+    }
 
+    for (int i = 0; i < notud_stok; i++) {
+        cout << strengur[i] << " - " << tala[i] << endl;
+    }
+    infile.close();
     return 0;
 }
