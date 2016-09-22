9b39c4c3f4e6332769deacbbd1478188c89676b7
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-22 08:40:21 +0000

    autocommit

--- lectures/lecture09_01/main.cpp
+++ lectures/lecture09_01/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
 
 using namespace std;
 
+void open_file(ifstream &infile, string thefilename);
+
 int main()
 {
+    ifstream infile;
+    string thefilename;
+    cin >> thefilename;
+    open_file(infile, thefilename);
 
+    infile.close();
     return 0;
 }
+
+void open_file(ifstream &infile, string thefilename) {
+    infile.open(thefilename.c_str());
+    if (infile.fail()) {
+        cout << "Can't open the file: " << thefilename << endl;
+        exit(1);
+    }
+}
+
