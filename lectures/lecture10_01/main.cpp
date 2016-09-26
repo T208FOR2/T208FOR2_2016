ec07f6c9d0c520df56048cd7018a360c6d262c65
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-26 13:53:35 +0000

    autocommit

--- lectures/lecture10_01/main.cpp
+++ lectures/lecture10_01/main.cpp
@@ -3,12 +3,15 @@
 
 using namespace std;
 
+void prenta();
 void prenta(int i);
 void prenta(string s);
 
 int main()
 {
-
+    prenta("hello");
+    prenta(42);
+    prenta();
 
     return 0;
 }
@@ -18,4 +21,8 @@ void prenta(int i) {
 }
 void prenta(string s) {
     cout << "I'm a string function: " << s << endl;
+}
+
+void prenta() {
+    cout << "empty print funciton" << endl;
 }
