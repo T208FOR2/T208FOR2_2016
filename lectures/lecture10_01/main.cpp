48ec331a06e3f87993f47126c04c3aba92c8d3a2
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-26 13:37:14 +0000

    autocommit

--- lectures/lecture10_01/main.cpp
+++ lectures/lecture10_01/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    string s;
+    getline(cin, s);
+
+    cout << "You wrote the sentence: " << endl;
+    cout << s << endl;
 
     return 0;
 }
