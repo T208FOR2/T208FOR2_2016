cd291a4a69a6befbc44d60fc29bd5f9810b190c9
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-29 08:41:27 +0000

    autocommit

--- lectures/lecture11_01/main.cpp
+++ lectures/lecture11_01/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
 
 int main()
@@ -8,7 +9,9 @@ int main()
     string s;
     getline(cin, s);
 
-
+    for (unsigned int i = 0; i < s.length(); i++) {
+        s[i] = tolower(s[i]);
+    }
 
     return 0;
 }
