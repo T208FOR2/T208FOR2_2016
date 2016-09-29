d37d9c155c5dd3e674c10ecac61f3b9ae6d1867b
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-29 08:55:36 +0000

    autocommit

--- lectures/lecture11_01/main.cpp
+++ lectures/lecture11_01/main.cpp
@@ -14,10 +14,13 @@ int main()
     }
 
     s[0] = toupper(s[0]);
+    cout << "01234567890123456789012345678901234567890" << endl;
+    cout << s << endl;
 
     unsigned int pos;
     pos = s.find_first_of(" ,.!?\"\t\\");
     while (pos < s.length()-1) {
+        cout << "index: " << pos << endl;
         s[pos+1] = toupper(s[pos+1]);
         pos = s.find_first_of(" ,.!?\"\t\\", pos+1);
     }
