84633e7ab62848e950bc50a9eef4a0edac767408
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-09-29 08:37:25 +0000

    autocommit

--- lectures/lecture11_01/main.cpp
+++ lectures/lecture11_01/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main()
 {
+    // Problem 1: Capitalize a sentence.
+    string s;
+    getline(cin, s);
+
+
 
     return 0;
 }
