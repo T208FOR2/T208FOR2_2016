318cb72d496edfd24f06113a117c967a26646e85
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-10-03 13:52:26 +0000

    autocommit

--- lectures/lecture12_01/main.cpp
+++ lectures/lecture12_01/main.cpp
@@ -15,8 +15,13 @@ int main()
 
     *p1 = 42;
 
+    p1 = p2;
+
     cout << "a = " << a << endl;
     cout << "b = " << b << endl;
+
+    cout << "*p1: " << *p1 << endl;
+    cout << "*p2: " << *p2 << endl;
 
     return 0;
 }
