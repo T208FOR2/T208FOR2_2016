d63b45a3294e5d1e51adacb6289370d17417e1a9
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-10-03 13:38:05 +0000

    autocommit

--- lectures/lecture12_01/main.cpp
+++ lectures/lecture12_01/main.cpp
@@ -4,10 +4,11 @@ using namespace std;
 
 int main()
 {
-    int a = 10;
+    int a[10];
 
-    cout << a << endl;
-    cout << &a << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << &a[i] << endl;
+    }
 
     return 0;
 }
