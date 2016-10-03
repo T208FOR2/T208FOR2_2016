df8be50efd2fcc8f710a4bf8f7451b1cbb52805f
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-10-03 14:02:32 +0000

    autocommit

--- lectures/lecture12_01/main.cpp
+++ lectures/lecture12_01/main.cpp
@@ -4,24 +4,7 @@ using namespace std;
 
 int main()
 {
-    int a = 83;
-    int b = 99;
 
-    int *p1;
-    int *p2 = &b;
-
-    p1 = &a;
-    p2 = &b;
-
-    *p1 = 42;
-
-    *p1 = *p2;
-
-    cout << "a = " << a << endl;
-    cout << "b = " << b << endl;
-
-    cout << "*p1: " << *p1 << endl;
-    cout << "*p2: " << *p2 << endl;
 
     return 0;
 }
