64f5b42643fde45cfb2dc978d202d63858a1aaa1
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-10-06 09:14:40 +0000

    autocommit

--- lectures/lecture13_02/main.cpp
+++ lectures/lecture13_02/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 
+// Problem 2: Read positive integers, until user writes a negative number,
+// then print the numbers out in reverse
+
 using namespace std;
 
 int main()
 {
+    int tala;
+    cin >> tala;
+    while (tala >= 0) {
+        // add to the array
+
+        cin >> tala;
+    }
+
+    // print in reverse
+    cout << "Hi!" << endl;
 
     return 0;
 }
