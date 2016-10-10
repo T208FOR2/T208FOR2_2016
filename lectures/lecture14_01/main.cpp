0a019384958bf42fe9981cba0214064e7cfe0238
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-10-10 13:19:29 +0000

    autocommit

--- lectures/lecture14_01/main.cpp
+++ lectures/lecture14_01/main.cpp
@@ -7,7 +7,20 @@ using namespace std;
 int main()
 {
     int *heimili = NULL;
+    int herbergi;
+    cin >> herbergi;
 
+    heimili = new int[herbergi];
+
+    for (int i = 0; i < herbergi; i++) {
+        cin >> heimili[i];
+    }
+
+    for (int i = 0; i < herbergi; i++) {
+        cout << heimili[i] << " ";
+    }
+
+    delete heimili;
 
     return 0;
 }
