24e91834de0265196605b34154f5b897a9943b4f
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-10-10 14:08:06 +0000

    autocommit

--- lectures/lecture14_02/main.cpp
+++ lectures/lecture14_02/main.cpp
@@ -2,8 +2,18 @@
 
 using namespace std;
 
+
+
 int main()
 {
+    int rows, columns;
+    cin >> rows >> columns;
+
+    int **p;
+    p = new int*[rows];
+    for (int i = 0; i < rows; i++) {
+        p[i] = new int[columns];
+    }
 
 
     return 0;
