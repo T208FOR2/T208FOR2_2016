56e63a96b8e831866de05ba1662595d9e5b415c2
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-10-13 09:50:55 +0000

    autocommit

--- lectures/lecture15_03/main.cpp
+++ lectures/lecture15_03/main.cpp
@@ -29,27 +29,16 @@ void printMovie(Movie m) {
     cout << ", born in " << m.directedby.birthyear << endl;
 }
 
-void testfunc(Movie &m) {
-    m.year = 9999;
-    m.title = "This thing has changed...!";
-}
-
 int main()
 {
     Director scott = {"Ridley Scott", 1937};
-    Movie m1, m2;
+    Movie m1;
 
     m1.title = "Alien";
     m1.year = 1979;
     m1.directedby = scott;
 
-
     printMovie(m1);
-    cout << endl;
-
-    m2 = m1;
-    printMovie(m2);
-    cout << endl;
 
     return 0;
 }
