886b3c58acdaa45d2bf7346c1378b40465be7499
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-10-13 09:22:25 +0000

    autocommit

--- lectures/lecture15_02/main.cpp
+++ lectures/lecture15_02/main.cpp
@@ -2,10 +2,24 @@
 
 using namespace std;
 
+struct Movie {
+    string title;
+    int year;
+};
 
+void printMovie(Movie m) {
+    cout << m.title << " (" << m.year << ")";
+}
 
 int main()
 {
+    Movie m1;
+
+    m1.title = "Alien";
+    m1.year = 1979;
+
+    printMovie(m1);
+    cout << endl;
 
     return 0;
 }
