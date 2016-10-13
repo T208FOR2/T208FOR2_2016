068c288ccc29951fb53f0ae42f11b5b3312ddf00
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-10-13 09:24:27 +0000

    autocommit

--- lectures/lecture15_02/main.cpp
+++ lectures/lecture15_02/main.cpp
@@ -13,12 +13,16 @@ void printMovie(Movie m) {
 
 int main()
 {
-    Movie m1;
+    Movie m1, m2;
 
     m1.title = "Alien";
     m1.year = 1979;
 
     printMovie(m1);
+    cout << endl;
+
+    m2 = m1;
+    printMovie(m2);
     cout << endl;
 
     return 0;
