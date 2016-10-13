177d7a01261f816c919126eda2d647e7fb5983bb
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-10-13 08:46:06 +0000

    autocommit

--- lectures/lecture15_01/main.cpp
+++ lectures/lecture15_01/main.cpp
@@ -2,9 +2,22 @@
 
 using namespace std;
 
+struct Statistics {
+    double average;
+    double stddev;
+    double min_value;
+    double max_value;
+};
+
+Statistics analyze_array(double *d, int n);
+
 int main()
 {
 
 
     return 0;
+}
+
+Statistics analyze_array(double *d, int n) {
+
 }
