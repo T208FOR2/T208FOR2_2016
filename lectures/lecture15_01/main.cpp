22747637b871346509a9a6c47a4695008507a52c
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-10-13 08:54:09 +0000

    autocommit

--- lectures/lecture15_01/main.cpp
+++ lectures/lecture15_01/main.cpp
@@ -13,8 +13,23 @@ Statistics analyze_array(double *d, int n);
 
 int main()
 {
+    Statistics s;
+    int n;
+    cin >> n;
+    double *p = new double[n];
 
+    for (int i = 0; i < n; i++) {
+        cin >> p[i];
+    }
 
+    s = analyze_array(p, n);
+
+    cout << "Min: " << s.min_value << endl;
+    cout << "Max: " << s.max_value << endl;
+    cout << "Average: " << s.average << endl;
+    cout << "StdDev: " << s.stddev << endl;
+
+    delete [] p;
     return 0;
 }
 
