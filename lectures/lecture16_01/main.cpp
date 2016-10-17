fa5c86ccb7ee119e4f568bfbe594dc39c5dc79c2
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-10-17 13:17:55 +0000

    autocommit

--- lectures/lecture16_01/main.cpp
+++ lectures/lecture16_01/main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 using namespace std;
 
+class Timi {
+public:
+    int klst;
+    int minutur;
+
+    void output();
+};
+
 int main()
 {
+    Timi t;
+
+    t.klst = 17;
+    t.minutur = 3;
+
+    t.output();
+    cout << endl;
 
     return 0;
 }
+
+
+void Timi::output() {
+    if (klst < 10) {
+        cout << "0";
+    }
+    cout << klst << ":";
+    if (minutur < 10) {
+        cout << "0";
+    }
+    cout << minutur;
+}
+
