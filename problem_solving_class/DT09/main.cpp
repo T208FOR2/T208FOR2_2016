967c8ec790f0792b93241aef277b6ee2f00b33c7
Sara Kristjánsdóttir <sarakri13@ru.is>
2016-10-18 10:11:11 +0000

    SantaBag lausn úr fyrri tíma

--- problem_solving_class/DT09/main.cpp
+++ problem_solving_class/DT09/main.cpp
@@ -6,17 +6,28 @@ class SantaBag
 {
 public:
     SantaBag();
+    SantaBag(string name, int hard, int soft);
     string getName();
     void setName(string name);
     void lesa();
     void skrifaUt();
+
     friend ostream &operator<<( ostream &out, const SantaBag &bag);
+    friend istream &operator>>( istream &in, SantaBag &bag);
+
+    friend bool operator>(const SantaBag &lhs, const SantaBag &rhs );
+    friend bool operator>=(const SantaBag &lhs, const SantaBag &rhs );
+    friend bool operator<(const SantaBag &lhs, const SantaBag &rhs );
+    friend bool operator==(const SantaBag &lhs, const SantaBag &rhs );
+
 
 
 private:
     string nafn;
     int hardir;
     int mjukir;
+
+    double reiknaGildi() const;
 
 };
 
@@ -27,12 +38,82 @@ SantaBag::SantaBag()
     mjukir = 0;
 }
 
+SantaBag::SantaBag(string name, int hard, int soft)
+{
+    nafn = name;
+    hardir = hard;
+    mjukir = soft;
+}
+
 ostream &operator<<( ostream &out, const SantaBag &bag)
 {
     out << "Nafn:" << bag.nafn << endl;
     out << "Hardir pakkar: " << bag.hardir << endl;
     out << "Mjukir pakkar: " << bag.mjukir << endl;
     return out;
+}
+
+istream &operator>>( istream &in, SantaBag &bag)
+{
+    cout << "Nafn: ";
+    in >> bag.nafn;
+
+    cout << "Harðid pakkar: ";
+    in >> bag.hardir;
+
+    cout << "Mjukir pakkar: ";
+    in >> bag.mjukir;
+
+    return in;
+}
+
+bool operator>(const SantaBag &lhs, const SantaBag &rhs )
+{
+    double lhsVal = lhs.reiknaGildi();
+    double rhsVal = rhs.reiknaGildi();
+    if(lhsVal > rhsVal)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool operator>=(const SantaBag &lhs, const SantaBag &rhs )
+{
+    double lhsVal = lhs.reiknaGildi();
+    double rhsVal = rhs.reiknaGildi();
+    if(lhsVal >= rhsVal)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool operator==(const SantaBag &lhs, const SantaBag &rhs )
+{
+    double lhsVal = lhs.reiknaGildi();
+    double rhsVal = rhs.reiknaGildi();
+    if(lhsVal == rhsVal)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool operator<(const SantaBag &lhs, const SantaBag &rhs )
+{
+    double lhsVal = lhs.reiknaGildi();
+    double rhsVal = rhs.reiknaGildi();
+    if(lhsVal < rhsVal)
+    {
+        return true;
+    }
+    return false;
+}
+
+double SantaBag::reiknaGildi() const
+{
+    return (hardir + mjukir*0.6);
 }
 
 void SantaBag::skrifaUt()
@@ -66,17 +147,41 @@ string SantaBag::getName()
 
 int main()
 {
-    SantaBag sveinki1;
-    sveinki1.skrifaUt();
+    SantaBag sveinki1("Sveinki1",1,2);
+    //sveinki1.skrifaUt();
 
-    sveinki1.setName("Stufur");
-    cout << sveinki1.getName() << endl;
+    //sveinki1.lesa();
+    //cout << sveinki1 << endl;
 
-    sveinki1.lesa();
-    sveinki1.skrifaUt();
+    SantaBag sveinki2("Sveinki2",1,2);
+    //cout << sveinki2;
 
-    cout << sveinki1 << endl;
+   // cin >> sveinki2;
+    cout << sveinki2;
+    cout << sveinki1;
 
+    if(sveinki1 > sveinki2)
+    {
+        cout << "Sveinki1" << endl;
+    }
+    else if( sveinki1 < sveinki2)
+    {
+        cout << "sveinki1 < sveinki2" << endl;
+    }
+    else
+    {
+        cout << "Sveinki2" << endl;
+    }
+
+    if(sveinki1 == sveinki2)
+    {
+        cout << " Eins" << endl;
+    }
+
+    if(sveinki1 >= sveinki2)
+    {
+        cout << "Sveinki2 >= Sveinki2" << endl;
+    }
 
     return 0;
 }
