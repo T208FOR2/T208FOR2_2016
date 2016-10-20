95100be943380824353a39046996c68cbdac2c36
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-10-20 09:04:25 +0000

    autocommit

--- lectures/lecture17_01/main.cpp
+++ lectures/lecture17_01/main.cpp
@@ -20,6 +20,7 @@ public:
     friend bool operator > (const Timi &lhs, const Timi &rhs);
 
     friend Timi operator + (const Timi &lhs, const Timi &rhs);
+    friend Timi operator - (const Timi &lhs, const Timi &rhs);
 
 private:
     int klst;
@@ -42,17 +43,13 @@ int main()
         cout << t2 << endl;
     }
 
-    Timi t3 = t1 + t2;
+    Timi t3 = t1 - t2;
     cout << t3 << endl;
 
     return 0;
 }
 
-___ operator + (const ___ &lhs, const ___ &rhs) {
-    ___ newstuff;
-    ...?
-    return newstuff;
-}
+
 
 Timi operator + (const Timi &lhs, const Timi &rhs) {
     Timi new_t;
@@ -64,6 +61,7 @@ Timi operator + (const Timi &lhs, const Timi &rhs) {
 
     return new_t;
 }
+
 
 bool operator > (const Timi &lhs, const Timi &rhs) {
     if (lhs.klst > rhs.klst) {
