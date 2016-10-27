22b980068f0bcb4dba8577e657decc12ee6fb5a3
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-10-27 09:00:49 +0000

    autocommit

---
+++ lectures/lecture19_02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int test() {
+    cout << "Hello!" << endl;
+    return 0;
+}
+
+int main()
+{
+    int x = test();
+
+    cout << x << endl;
+
+    return 0;
+}
