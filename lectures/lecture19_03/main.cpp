6d6d34e24b93a92023813166154dd59cd7206d61
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-10-27 09:41:15 +0000

    autocommit

--- lectures/lecture19_03/main.cpp
+++ lectures/lecture19_03/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
+
+class GroupProject {
+public:
+
+    bool is_member(string nafn);
+
+    void sortgroup();
+
+private:
+    int n;
+    string *p;
+};
+
+GroupProject::GroupProject() {
+    n = 0;
+    p = NULL;
+}
+
+void GroupProject::sortgroup() {
+  int i, j;
+  string tmp;
+
+  for (i = 1; i < n; i++) {
+    tmp = p[i];
+    j = i;
+    while (j > 0 && p[j - 1] > tmp) {
+      p[j] = p[j - 1];
+      j--;
+    }
+    p[j] = tmp;
+  }
+}
 
 int main()
 {
