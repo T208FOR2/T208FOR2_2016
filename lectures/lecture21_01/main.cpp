d6907e3da0f39c90595716d4455b2c4603424fcc
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-11-03 09:08:58 +0000

    autocommit

--- lectures/lecture21_01/main.cpp
+++ lectures/lecture21_01/main.cpp
@@ -2,9 +2,44 @@
 
 using namespace std;
 
+class Person {
+public:
+    string nafn;
+
+    Person* firstChild;
+    Person* lastChild;
+
+    Person* nextSibling;
+    Person* prevSibling;
+
+    Person* parent;
+
+    Person();
+    Person(string personname);
+};
+
+
 int main()
 {
 
 
     return 0;
+}
+
+Person::Person() {
+    parent = NULL;
+    prevSibling = NULL;
+    nextSibling = NULL;
+    firstChild = NULL;
+    lastChild = NULL;
+    nafn = "";
+}
+
+Person::Person(string personname) {
+    parent = NULL;
+    prevSibling = NULL;
+    nextSibling = NULL;
+    firstChild = NULL;
+    lastChild = NULL;
+    nafn = personname;
 }
