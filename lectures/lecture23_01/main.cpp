f0c573bd47d73b94f1cb5b2c4ddb4497ad560d4a
Eyjolfur Ingi Asgeirsson <eyjolfur.asgeirsson@gmail.com>
2016-11-10 08:34:10 +0000

    autocommit

--- lectures/lecture23_01/main.cpp
+++ lectures/lecture23_01/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
-
 using namespace std;
+const int N = 10;
 
-int main()
-{
+struct Node {
+    int data;
+    Node* next;
+};
 
+int main() {
+    Node* head = NULL, *tmp;
+
+    for (int i = 0; i < N; i++) {
+        tmp = new Node;
+        tmp->data = N - i;
+        tmp->next = head;
+        head = tmp;
+    }
+
+    // ADD CODE HERE...
+
+
+
+    // END OF ADDED CODE
+
+    tmp = head;
+    while (tmp != NULL) {
+        cout << tmp->data << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+
+    while (head != NULL) {
+        tmp = head;
+        head = head->next;
+        delete tmp;
+    }
     return 0;
 }
